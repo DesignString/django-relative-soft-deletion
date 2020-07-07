7f074194cea54e1127db9f3942e0fd92cb32e770
Murugan D <yasmuru@users.noreply.github.com>
2020-07-07 08:46:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,29 @@
 from setuptools import setup
 
-setup()
+setup(
+  name = django-relative-softdeletion
+  version = 0.1
+  description = A Django models extension to add soft deletion functionality.
+  long_description = file: README.rst
+  url = https://www.designstring.com/
+  author = Designstring
+  author_email = admin@designstring.com
+  license = MIT
+  classifiers = [
+    Environment :: Web Environment
+    Framework :: Django
+    Framework :: Django :: X.Y  # Replace "X.Y" as appropriate
+    Intended Audience :: Developers
+    License :: OSI Approved :: BSD License
+    Operating System :: OS Independent
+    Programming Language :: Python
+    Programming Language :: Python :: 3
+    Programming Language :: Python :: 3 :: Only
+    Programming Language :: Python :: 3.6
+    Programming Language :: Python :: 3.7
+    Programming Language :: Python :: 3.8
+    Topic :: Internet :: WWW/HTTP
+    Topic :: Internet :: WWW/HTTP :: Dynamic Content
+  ]
+
+)
