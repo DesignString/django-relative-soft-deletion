6fa5c523b39384c238639cf9b7803ad886f8a40d
Murugan D <yasmuru@gmail.com>
2020-07-10 10:55:25 +0530

    Update home page url

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r") as fh:
     
 setup(
   name="django-relative-softdeletion",
-  version= "0.0.1",
+  version= "0.0.2",
   description="A Django models extension to add soft deletion functionality",
   long_description=long_description,
   long_description_content_type="text/markdown",
-  url="https://www.designstring.com/",
+  url="https://github.com/DesignString/django-relative-soft-deletion",
   author="Designstring",
   author_email="admin@designstring.com",
   license ="MIT",
